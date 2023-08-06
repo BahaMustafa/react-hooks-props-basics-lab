190ecdaa21c6d3e271e987a3896dd15c1f66a6b1
= <=>
2023-08-06 12:04:10 -0700

    Props Basics Lab

--- src/components/App.js
+++ src/components/App.js
@@ -3,16 +3,15 @@ import NavBar from "./NavBar";
 import Home from "./Home";
 import About from "./About";
 
-// pass this data down as props to the child component(s) that need it!
-import user from "../data/user";
-console.log(user);
+import user from "../data/user.js";
+
 
 function App() {
   return (
     <div>
       <NavBar />
-      <Home />
-      <About />
+      <Home color="firebrick" name={user.name} city={user.city} />
+      <About bio={user.bio} github={user.links.github} linkedin={user.links.linkedin} />
     </div>
   );
 }
